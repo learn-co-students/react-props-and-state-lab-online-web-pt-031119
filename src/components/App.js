a58de64bf2a6f97e647c60be69582c714915cc6b
Michael Schiumo <mschiumo18@gmail.com>
2019-12-18 14:19:05 -0500

    before router change

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,36 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.onFindPetsClick();
+  }
+
+
+
+  onFindPetsClick = () => {
+      return fetch('/api/pets')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            this.setState({ data : responseJson })
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
+
+  onChangeType = () => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: this.state.type
+      }
+    })
+  }
+
   render() {
     return (
+
+    // <Router>
       <div className="ui container">
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
@@ -24,7 +52,8 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPetsClick={this.onFindPetsClick} onChange={this.onChangeType} />
+
             </div>
             <div className="twelve wide column">
               <PetBrowser />
@@ -32,6 +61,7 @@ class App extends React.Component {
           </div>
         </div>
       </div>
+    // </Router>
     )
   }
 }
